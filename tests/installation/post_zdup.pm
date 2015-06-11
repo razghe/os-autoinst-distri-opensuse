9ec0da1793a20dd5b780634783fcb391bf1f5787
razghe <rgherlea@suse.com>
2015-06-11 11:40:10 +0200

    Update post_zdup.pm
    Code was added to serve the noautologin parameter.

--- tests/installation/post_zdup.pm
+++ tests/installation/post_zdup.pm
@@ -27,6 +27,19 @@ sub run() {
     assert_screen "grub2", 50;
 
     # Wait until the point that consoletests can start working
+        if ( get_var("NOAUTOLOGIN") ) {
+        my $ret = assert_screen 'displaymanager', 200;
+        if ( get_var('DM_NEEDS_USERNAME') ) {
+            type_string $username;
+        }
+        else {
+            send_key "ret";
+            wait_idle;
+        }
+        type_string "$password";
+        send_key "ret";
+    }
+    # Wait until the point that consoletests can start working
     assert_screen "desktop-at-first-boot", 400;
 }
 
